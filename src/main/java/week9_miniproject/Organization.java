715616d124365c3a937cf8b5798036dd9a2967fe
alinaberlin <alina.ghetler@gmail.com>
2023-10-31 15:49:21 +0100

    Create manager node, child left node and right child node

--- src/main/java/week9_miniproject/Organization.java
+++ src/main/java/week9_miniproject/Organization.java
@@ -1,16 +1,36 @@
 package week9_miniproject;
 
 public class Organization {
-    class Node{
+    class Node {
         Employeee value;
         Node left;
         Node right;
 
         public Node(Employeee value) {
             this.value = value;
-            left  = null;
+            left = null;
             right = null;
         }
     }
 
+    private Node manager;
+
+    public void add(Employeee employee) {
+        if (manager == null) {
+            manager = new Node(employee);
+        } else {
+            Node current = manager;
+            while(current != null) {
+                if (current.left == null) {
+                    current.left = new Node(employee);
+                    break;
+                } else if (current.right == null) {
+                    current.right = new Node(employee);
+                    break;
+                } else {
+                    current = current.left;
+                }
+            }
+        }
+    }
 }
